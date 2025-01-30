6a576db551f738d85b445826c70b5cbfa8452867
MARLMG <152422770+MAR-GUEZ@users.noreply.github.com>
2025-01-29 20:15:27 -0500

    Enhance cart functionality by adding item quantity management and removal logic

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const itemInCart = state.items.find((item) => item.name === name);
+      if (itemInCart) {
+        itemInCart.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.name !== action.payload.name
+      );
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const itemInCart = state.items.find((item) => item.name === name);
+      if (itemInCart) {
+        itemInCart.quantity = quantity;
+      }
     },
   },
 });
